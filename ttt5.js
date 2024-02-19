acf1fa3316f5ff89f22dda92083707d238a4a42a
tsflores <trinidad.s.flores@gmail.com>
2024-02-19 18:38:09 -0500

    Added ability to identify winner

--- ttt5.js
+++ ttt5.js
@@ -17,7 +17,8 @@ document.addEventListener("DOMContentLoaded", function () {
     var array = ['sq0', 'sq1', 'sq2', 'sq3', 'sq4', 'sq5', 'sq6', 'sq7', 'sq8'];
     options = "OX".split('');
     var moves = [];
-    var winConditions = [ ];
+    var winConditions = [];
+    var winStatus = false;
 
     startGame();
 
@@ -33,20 +34,27 @@ document.addEventListener("DOMContentLoaded", function () {
             selectedSquare = document.getElementById(this.id);
 
             // conditional statement to make sure a square doesn't already have an X or an O
-            if (selectedSquare.textContent == '') {
+            if (selectedSquare.textContent == '' && !winStatus) {
 
                 // use a set of conditional statements to make selections for next player, color, etc...
                 selectedSquare.style.color = (currentPlayer == "O") ? "red" : "black";
                 selectedSquare.innerHTML = currentPlayer;
                 playerTurn.innerText = (currentPlayer == "O") ? "It's X's Turn" : "It's O's Turn";
                 currentPlayer = (currentPlayer == "X") ? "O" : "X";
-                
+
                 mapContents();
-                checkWinner(moves);
-                console.log(winConditions);
+                winStatus = checkWinner(moves);
+                if(winStatus){
+                    console.log(winStatus);
+                    selectedSquare.removeEventListener("click", function (){console.log("Trying to remove event listener");}, false);
+                }
             }
             else if (checkStalemate(moves)) {
                 alert("Game Over:  Please select Start Game if you wish to play again");
+            }
+            else if(winStatus){
+                tttSquares[i].disable;
+                alert(playerTurn.innerText.substring(0,1) + " has won. To play again, select the Start Game button");
             }
             else {
                 alert("Square " + (i + 1) + " is taken and no longer available for play!"); //reference square number from index i in for loop
@@ -84,13 +92,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 return "-"; //if no X or O, replace with a -
             }
         });
-        // moves = moves.join("");
     };
 
     //checks to see if any '-' still exist in the board array
     function checkStalemate(outcome) {
         count = 0;
-        // outcome = outcome.split('');
         outcome.forEach(function (letter) {
             if (options.includes(letter)) {
                 count++;
@@ -102,15 +108,55 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    // couldn't get to an analytical solution to the pattern so create an array that holds the square numbers of the winning positions
     function checkWinner(currentBoard) {
-        var dummy ="";
-        for(i = 0; i<3; i++) {
-            for (j = 0; j<3; j++) {
-           dummy = dummy + currentBoard[i+j];
-           console.log(dummy);
+        arrWinningPositions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+        currentBoard = currentBoard.join("");
+        placeHolder = "";
+        winner = false;
+
+        for (i = 0; i < 8; i++) {
+            for (j = 0; j < 3; j++) {
+                placeHolder += currentBoard.charAt(arrWinningPositions[i][j]); //use the charAt method to get characters on the board
+            }
+            winConditions[i] = placeHolder;
+            placeHolder = "";
         }
-        winConditions[i] = dummy;
+        winConditions.forEach(function (combo) {
+            if (combo == "XXX") {
+                playerTurn.innerText = "X Wins!!!!"
+                winner = true;
+            }
+            else if (combo == "OOO") {
+                playerTurn.innerText = "O Wins!!!!"
+                winner = true;
+            }
+        })
+        return winner;
     }
 
-    };
+    //my attempt at looking for an anlytical solution to the winConditions array but got stuck on the 2nd and 3rd column pattern
+    // function checkWinner(currentBoard) {
+    //     var placeHolder = "";
+    //     var k = [0, 2, 4, 0, 1, 2, 0, 0];
+    //     var index;
+    //     for (i = 0; i < 7; i++) {
+    //         if (i < 3) {
+    //             for (j = 0; j < 3; j++) {
+    //                 placeHolder += currentBoard[i + j + k[i]];
+    //             }
+    //         }
+    //         else if (i > 2 && i < 6) {
+    //             for (j = 0; j <3; j++) {
+    //                 index = i*j+k[i];
+    //                 placeHolder += currentBoard[index];
+    //             }
+
+    //         }
+    //         winConditions[i] = placeHolder;
+    //         console.log(winConditions);
+    //         placeHolder = "";
+    //     }
+
+    // };
 });
